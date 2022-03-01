052f87cc3651732fbd44312bda56e38c915741e7
Alexandre Caumette <alexandre.caumette@gmail.com>
2022-03-01 17:29:02 +0100

    tried to make the algo work with a simple example (failed)

--- methaeuristics/ag.py
+++ methaeuristics/ag.py
@@ -1,57 +1,65 @@
 # Problem definition :
 import random as rd
+import numpy as np
 
-PROBA_CROISEMENT = 0.2
+PROBA_CROSSING = 0.2
 PROBA_MUTATION = 0.1
-MAX_ITERATION = 10
+MAX_ITERATION = 20
 PENALTY = 2
+COST_MATRIX = np.array([
+    [0, 1, 2, 1],
+    [1, 0, 3, 6],
+    [5, 2, 0, 1],
+    [1, 3, 7, 0],
+])
 
-# Initial population
-
-iteration = 0
-population = []
-N = len(population)
 
 # Functions
 
-def nbrOfVehicles(individual):
+def nbr_of_vehicles(individual):
     return 1
 
 
-def cost(i, j):
-    return i + j
-
-
 def fitness(individual):
-    vehicleCost = PENALTY * nbrOfVehicles(individual)
-    travelCost = 0
+    vehicle_cost = PENALTY * nbr_of_vehicles(individual)
+    travel_cost = 0
+    n = len(individual) - 1
 
-    for index in range(len(individual)):
+    for index in range(n):
         i = individual[index]
         j = individual[index + 1]
-        travelCost += cost(i, j)
+        travel_cost += COST_MATRIX[i, j]
 
-    return vehicleCost + travelCost
+    return vehicle_cost + travel_cost
 
 
 def mutate(individual):
-    i = rd.randint(0, len(individual))
-    j = rd.randint(i, len(individual))
+    n = len(individual) - 1
+    i = rd.randint(0, n - 1)
+    j = rd.randint(i + 1, n)
 
-    result = individual[:i] + individual[j] + individual[i + 1: j] + individual[i] + individual[j + 1:]
+    result = individual[:i] + \
+             [individual[j]] + \
+             individual[i + 1: j] + \
+             [individual[i]] + \
+             individual[j + 1:]
+
     return result
+
 
 # Individual crossing functions
 
-def onePointCrossOver(father, mother):
-    point = rd.randint(0, len(father))
+def one_point_cross_over(father, mother):
+    n = len(father) - 1
+    point = rd.randint(0, n)
 
-    brother = father[:point] + mother[point + 1:]
-    sister = mother[:point] + father[point + 1:]
+    brother = father[:point] + mother[point:]
+    sister = mother[:point] + father[point:]
 
     return [brother, sister]
 
-def twoPointsCrossOver(father, mother):
+
+def two_points_cross_over(father, mother):
     point_1 = rd.randint(0, len(father))
     point_2 = rd.randint(point_1, len(father))
 
@@ -60,7 +68,8 @@ def twoPointsCrossOver(father, mother):
 
     return [brother, sister]
 
-def uniformCrossOver(father, mother):
+
+def uniform_cross_over(father, mother):
     mask = []
     for index in range(len(father)):
         mask.append(rd.randint(0, 1))
@@ -69,52 +78,74 @@ def uniformCrossOver(father, mother):
     sister = []
 
     for index in range(len(mask)):
-        if (mask[index]) :
+        if mask[index]:
             brother.append(father[index])
             sister.append(mother[index])
-        else :
+        else:
             brother.append(mother[index])
             sister.append(father[index])
 
     return [brother, sister]
 
+
 # Main algorithm :
 
-while iteration < MAX_ITERATION:
-    iteration += 1
+def main():
+    # Initialization
+    iteration = 0
+    population = [
+        [0, 1, 2, 3],
+        [0, 1, 3, 2],
+        [0, 2, 3, 1],
+        [0, 2, 1, 3],
+        [0, 3, 1, 2],
+        [0, 3, 2, 1],
+        [1, 2, 0, 3],
+        [2, 1, 0, 3],
+    ]
+    N = len(population)
 
-    # determinist method
-    population = sorted(population, key=lambda x: fitness(x))
+    while iteration < MAX_ITERATION:
+        iteration += 1
 
-    # stochastic method (technique de la roulette)
-    # population = stochasticSort(population)
+        # determinist method
+        population = sorted(population, key=lambda x: fitness(x))
 
-    # survivors selection
-    population = population[:N]
-    rd.shuffle(population)
+        # stochastic method (technique de la roulette)
+        # population = stochasticSort(population)
 
-    # individuals cross
-    populationNext = []
+        # survivors selection
+        population = population[:N]
+        rd.shuffle(population)
 
-    for index in range(0, len(population) // 2, 2):
-        father = population[index]
-        mother = population[index + 1]
+        # individuals cross
+        population_next = []
 
-        if rd.random() < PROBA_CROISEMENT:
-            children = onePointCrossOver(father, mother)
-            populationNext.append(children)
-        else:
-            populationNext.append(father)
-            populationNext.append(mother)
+        for index in range(0, len(population) // 2, 2):
+            father = population[index]
+            mother = population[index + 1]
 
-    # individuals mutation
-    population = []
+            if rd.random() < PROBA_CROSSING:
+                children = one_point_cross_over(father, mother)
+                population_next.append(children[0])
+                population_next.append(children[1])
+            else:
+                population_next.append(father)
+                population_next.append(mother)
 
-    for index in range(len(populationNext)):
-        element = populationNext[index]
+        # individuals mutation
+        population = []
 
-        if rd.random() < PROBA_MUTATION:
-            mutatedElement = mutate(element)
-            population.append(mutatedElement)
-        else:
-            population.append(element)
+        for index in range(len(population_next)):
+            element = population_next[index]
+
+            if rd.random() < PROBA_MUTATION:
+                mutated_element = mutate(element)
+                population.append(mutated_element)
+            else:
+                population.append(element)
+
+        print('Iteration {} : {}'.format(iteration, population))
+
+
+main()
