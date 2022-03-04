e02a76ac5a6fb90c4b303000e190f4364c8c91b1
flavianferre <100682461+flavianferre@users.noreply.github.com>
2022-03-04 15:02:36 +0100

    Modification de la matrice de coût grâce aux xls

--- methaeuristics/genetic algorithm.py
+++ methaeuristics/genetic algorithm.py
@@ -1,5 +1,6 @@
 import random as rd
 import numpy as np
+import xlrd as xl
 
 
 class GeneticAlgorithm:
@@ -9,10 +10,31 @@ class GeneticAlgorithm:
     PENALTY: int = 2
     POPULATION_SIZE = 10
 
-    def __init__(self, max_iteration=15, nbr_of_sites=10):
+    def __init__(self, max_iteration=15, nbr_of_sites=107):
         self.MAX_ITERATION = max_iteration
         self.NBR_OF_SITES = nbr_of_sites
-        self.COST_MATRIX = np.random.randint(1, 20, size=(nbr_of_sites, nbr_of_sites))
+        doc_dep = xl.open_workbook("4_detail_table_depots.xls")
+        feuille_dep = doc_dep.sheet_by_index(0)
+        lat_dep = feuille_dep.cell_value(rowx=1,colx=3)
+        long_dep = feuille_dep.cell_value(rowx=1,colx=4)
+        doc_cust = xl.open_workbook("2_detail_table_customers.xls")
+        feuille_cust = doc_cust.sheet_by_index(0)
+        self.COST_MATRIX = np.zeros(size=(nbr_of_sites, nbr_of_sites))
+        for i in range(1,108):
+            lat_cust = feuille_cust.cell_value(rowx=i,colx=3)
+            long_cust = feuille_cust.cell_value(rowx=i,colx=4)
+            self.COST_MATRIX[0][i]=np.sqrt((lat_cust-lat_dep)**2 + (long_cust-long_dep)**2)*0.2
+        for i in range(2,108):
+            for j in range(i+1,108):
+                lat_i = feuille_cust.cell_value(rowx=i,colx=3)
+                long_i = feuille_cust.cell_value(rowx=i,colx=4)
+                lat_j = feuille_cust.cell_value(rowx=j,colx=3)
+                long_j = feuille_cust.cell_value(rowx=j,colx=4)
+                dist = np.sqrt((lat_i-lat_j)**2 + (long_i-long_j)**2)
+                self.cost_MATRIX[i][j]=dist*0.2
+                self.cost_MATRIX[j][i]=dist*0.2
+
+
 
     def main(self):
         # Initialization
